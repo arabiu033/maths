eb94634bb642b02aa73dc62dc921b51b098f27d6
Aminu Rabiu <arabiu033@gmail.com>
2022-10-25 17:42:03 +0100

    Rewrite app.js to class base component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import Calc from './components/Calculator';
 
-function App() {
-  return (
-    <Calc />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calc />
+    );
+  }
 }
 
 export default App;
