dd3f52b7ca157900da8963f6189daff47db74ee9
Aminu Rabiu <arabiu033@gmail.com>
2022-10-25 14:32:11 +0100

    Make the necessary changes before starting

---
+++ src/components/Calculator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Calc extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+export default Calc;
