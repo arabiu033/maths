ab31128706f8757708cfbbd4b4e90c569bc090fd
Aminu Rabiu <arabiu033@gmail.com>
2022-10-25 15:54:14 +0100

    Add the code for the calc js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Rows from './Rows';
+import Display from './Display';
 
 class Calc extends React.Component {
   constructor() {
@@ -8,7 +10,16 @@ class Calc extends React.Component {
 
   render() {
     return (
-      <h1>Hello World!</h1>
+      <main>
+        <Display />
+        <div>
+          <div><Rows row={['Ac', '+/-', '%', '/']} /></div>
+          <div><Rows row={['7', '8', '9', 'X']} /></div>
+          <div><Rows row={['4', '5', '6', '-']} /></div>
+          <div><Rows row={['1', '2', '3', '+']} /></div>
+          <div><Rows row={['0', '.', '=']} /></div>
+        </div>
+      </main>
     );
   }
 }
