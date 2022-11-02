2e5db4a37ef69686d7bf7cfe3b610d81ace7ac27
Aminu Rabiu <arabiu033@gmail.com>
2022-11-02 14:28:19 +0100

    Serve as the main function for all the components to meet together

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Quote from './components/Quote';
+import Home from './components/Home';
+import Header from './components/Header';
 import Calc from './components/Calculator';
 
 function App() {
   return (
-    <Calc />
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calc />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </>
   );
 }
 
