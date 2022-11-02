8fd4a603633f063b1c6f7d599e99e36796f9d2c5
Aminu Rabiu <arabiu033@gmail.com>
2022-11-02 14:29:56 +0100

    Add the component responsible for the quote display

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/quote.css';
+
+const Quote = () => (
+  <div className="quote">
+    <h3>
+      Mathematics is not about numbers, equations, computations,
+      <br />
+      or algorithms: it is about understanding. -- William Paul Thurston
+    </h3>
+  </div>
+);
+
+export default Quote;
