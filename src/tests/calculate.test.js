d53dc19ac5abb8a0b8aa21c6304e38fb4d3e8bc9
Aminu Rabiu <arabiu033@gmail.com>
2022-11-03 10:46:06 +0100

    All tests are passing

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,62 +1,57 @@
 import calculate from '../logic/calculate';
-​
+
 describe('Addition', () => {
-​
   test('First', () => {
-    expect(calculate({total: null, next: null, operation: null,}, 'AC')).toStrictEqual({total: null, next: null, operation: null});
-  })
-​
+    expect(calculate({ total: null, next: null, operation: null }, 'AC')).toStrictEqual({ total: null, next: null, operation: null });
+  });
+
   test('Second', () => {
-    expect(calculate({total: '50', next: null, operation: '+',}, '5')).toStrictEqual({total: '50', next: '5', operation: '+'});
-  })
-​
+    expect(calculate({ total: '50', next: null, operation: '+' }, '5')).toStrictEqual({ total: '50', next: '5', operation: '+' });
+  });
+
   test('Third', () => {
-    expect(calculate({total: '50', next: '50', operation: '+',}, '=')).toStrictEqual({total: '100', next: null, operation: null});
-  })
-})
-​
+    expect(calculate({ total: '50', next: '50', operation: '+' }, '=')).toStrictEqual({ total: '100', next: null, operation: null });
+  });
+});
+
 describe('Subtraction', () => {
-​
   test('First', () => {
-    expect(calculate({total: null, next: null, operation: null,}, '.')).toStrictEqual({total: null, next: '0.', operation: null});
-  })
-​
+    expect(calculate({ total: null, next: null, operation: null }, '.')).toStrictEqual({ total: null, next: '0.', operation: null });
+  });
+
   test('Second', () => {
-    expect(calculate({total: '10', next: null, operation: '-',}, '5')).toStrictEqual({total: '10', next: '5', operation: '-'});
-  })
-​
+    expect(calculate({ total: '10', next: null, operation: '-' }, '5')).toStrictEqual({ total: '10', next: '5', operation: '-' });
+  });
+
   test('Third', () => {
-    expect(calculate({total: '50', next: '50', operation: '-',}, '=')).toStrictEqual({total: '0', next: null, operation: null});
-  })
-})
+    expect(calculate({ total: '50', next: '50', operation: '-' }, '=')).toStrictEqual({ total: '0', next: null, operation: null });
+  });
+});
 
 describe('Division', () => {
-​
   test('First', () => {
-    expect(calculate({total: null, next: null, operation: null,}, '%')).toStrictEqual({operation: '%'});
-  })
-​
+    expect(calculate({ total: null, next: null, operation: null }, '%')).toStrictEqual({ operation: '%' });
+  });
+
   test('Second', () => {
-    expect(calculate({total: 10, next: null, operation: '÷',}, '5')).toStrictEqual({total: 10, next: '5', operation: '÷'});
-  })
-​
+    expect(calculate({ total: 10, next: null, operation: '÷' }, '5')).toStrictEqual({ total: 10, next: '5', operation: '÷' });
+  });
+
   test('Third', () => {
-    expect(calculate({total: '50', next: '50', operation: '÷',}, '=')).toStrictEqual({total: '1', next: null, operation: null});
-  })
-})
+    expect(calculate({ total: '50', next: '50', operation: '÷' }, '=')).toStrictEqual({ total: '1', next: null, operation: null });
+  });
+});
 
 describe('Negation', () => {
-​
   test('First', () => {
-    expect(calculate({total: null, next: null, operation: null,}, '+/-')).toStrictEqual({});
-  })
-​
-  test('Second', () => {
-    expect(calculate({total: '10', next: '5', operation: '+',}, '+/-')).toStrictEqual({total: '10', next: '-5', operation: '+'});
-  })
-​
-  test('Third', () => {
-    expect(calculate({total: null, next: '50', operation: '+',}, '+/-')).toStrictEqual({total: null, next: '-50', operation: '+'});
-  })
-})
+    expect(calculate({ total: null, next: null, operation: null }, '+/-')).toStrictEqual({});
+  });
 
+  test('Second', () => {
+    expect(calculate({ total: '10', next: '5', operation: '+' }, '+/-')).toStrictEqual({ total: '10', next: '-5', operation: '+' });
+  });
+
+  test('Third', () => {
+    expect(calculate({ total: null, next: '50', operation: '+' }, '+/-')).toStrictEqual({ total: null, next: '-50', operation: '+' });
+  });
+});
