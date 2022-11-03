02b022dff071b2afc3d728ded812d916104ac676
Aminu Rabiu <arabiu033@gmail.com>
2022-11-03 10:23:41 +0100

    Add a unit test for the operate function

---
+++ src/tests/operate.test.js
@@ -0,0 +1,17 @@
+import operate from "../logic/operate";
+
+test('+', () => {
+  expect(operate('5', '5', '+')).toBe("10");
+})
+
+test('-', () => {
+  expect(operate('5', '5', '-')).toBe("0");
+})
+
+test('/', () => {
+  expect(operate('5', '5', '÷')).toBe("1");
+})
+
+test('x', () => {
+  expect(operate('5', '5', 'x')).toBe("25");
+})
