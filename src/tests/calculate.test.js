36bb26a7d1901c445eda9fd1aa47ef9656d7d846
Temesgen Ayneka <temesgen.ayneka@gmail.com>
2022-11-03 12:40:03 +0300

    Test code for division

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -30,3 +30,18 @@ describe('Subtraction', () => {
   })
 })
 
+describe('Division', () => {
+​
+  test('First', () => {
+    expect(calculate({total: null, next: null, operation: null,}, '%')).toStrictEqual({operation: '%'});
+  })
+​
+  test('Second', () => {
+    expect(calculate({total: 10, next: null, operation: '÷',}, '5')).toStrictEqual({total: 10, next: '5', operation: '÷'});
+  })
+​
+  test('Third', () => {
+    expect(calculate({total: '50', next: '50', operation: '÷',}, '=')).toStrictEqual({total: '1', next: null, operation: null});
+  })
+})
+
