3cfcf8bb953afd688e0efff449c4d9960ee5b53f
Aminu Rabiu <arabiu033@gmail.com>
2022-11-03 10:27:41 +0100

    Install necessary dependencies and fix linters

--- src/tests/components.test.js
+++ src/tests/components.test.js
@@ -12,32 +12,32 @@ it('render correctly Display component', () => {
     total: null,
     next: null,
     operation: null,
-  }
-  const display = renderer.create(<Display obj={obj}/>).toJSON();
+  };
+  const display = renderer.create(<Display obj={obj} />).toJSON();
   expect(display).toMatchSnapshot();
-})
+});
 
 it('render correctly Header component', () => {
   const header = renderer.create(<BrowserRouter><Header /></BrowserRouter>).toJSON();
   expect(header).toMatchSnapshot();
-})
+});
 
 it('render correctly Quote component', () => {
   const quote = renderer.create(<Quote />).toJSON();
   expect(quote).toMatchSnapshot();
-})
+});
 
 it('render correctly Home component', () => {
   const home = renderer.create(<Home />).toJSON();
   expect(home).toMatchSnapshot();
-})
+});
 
 it('render correctly Rows component', () => {
-  const row = renderer.create(<Rows row={["1", "2", "3"]} setObj={()=>{}}/>).toJSON();
+  const row = renderer.create(<Rows row={['1', '2', '3']} setObj={() => {}} />).toJSON();
   expect(row).toMatchSnapshot();
-})
+});
 
 it('render correctly Calc component', () => {
   const calc = renderer.create(<Calc />).toJSON();
   expect(calc).toMatchSnapshot();
-})
+});
--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,17 +1,17 @@
-import operate from "../logic/operate";
+import operate from '../logic/operate';
 
 test('+', () => {
-  expect(operate('5', '5', '+')).toBe("10");
-})
+  expect(operate('5', '5', '+')).toBe('10');
+});
 
 test('-', () => {
-  expect(operate('5', '5', '-')).toBe("0");
-})
+  expect(operate('5', '5', '-')).toBe('0');
+});
 
 test('/', () => {
-  expect(operate('5', '5', '÷')).toBe("1");
-})
+  expect(operate('5', '5', '÷')).toBe('1');
+});
 
 test('x', () => {
-  expect(operate('5', '5', 'x')).toBe("25");
-})
+  expect(operate('5', '5', 'x')).toBe('25');
+});
